8a0c6f7d175025884f8763e9ced01d9c55a99538
Alex Roman <Alex@romanink.net>
2016-05-23 18:10:09 -0700

    Complete next step for data cleaning
    - Edit compress.pyp

--- scripts/compress.py
+++ scripts/compress.py
@@ -7,10 +7,33 @@ lines.pop(0) # pop first (legend)
 # previous = lines.pop(0).split(',') # init the first point
 
 with open(os.getcwd()+'/compressed.csv', 'a') as f:
-	# f.write(','.join(previous)) # write first
+	last = lines[0].split(',')
+	lastWasOnline = last[3] == '3'
+
+	first = last
+	# delete first
+	del lines[0]
+
+	if lastWasOnline:
+		del first[3:]
+		f.write(','.join(first))
+
+
 	for line in lines:
+
 		split = line.split(',')
-		# print(split[3])
-		if split[3] == '3':
-			del split[4:]
-			f.write(','.join(split) + '\n')
+		isOnline = split[3] == '3'
+
+		# offline to online
+		if isOnline and not lastWasOnline:
+			# looking for another '3'
+			del split[3:]
+			f.write(','.join(split))
+		# online to offline
+		if not isOnline and lastWasOnline:
+			# finish the last one
+			f.write(',' + last[2] + '\n')
+
+
+		last = split
+		lastWasOnline = isOnline
