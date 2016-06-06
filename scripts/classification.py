15052f7312c876225978021c369e9e946bdb17d2
Alex Roman <Alex@romanink.net>
2016-06-05 20:48:29 -0700

    Add classification script

--- scripts/classification.py
+++ scripts/classification.py
@@ -1,11 +1,24 @@
+import sys
+print('classifying ' + sys.argv[1])
 
-with open('./final.csv', 'r') as f:
+with open(sys.argv[1], 'r') as f:
 	lines = f.readlines()
 lines.pop(0)
 
+with open('./classifications.csv', 'a') as f:
+	f.write('user,total,totalNight1,totalMorning1,totalNight2,totalMorning2,totalNight3,totalMorning3,totalNight4,totalMorning4\n') 
+
+
+
 total = 0 # 24 hrs
-totalNight = 0 # 10pm-4am 1459832400-1459854000
-totalMorning = 0 # 6am - 11:59am 1459861200-1459882800
+totalNight1 = 0
+totalMorning1 = 0
+totalNight2 = 0
+totalMorning2 = 0
+totalNight3 = 0
+totalMorning3 = 0
+totalNight4 = 0
+totalMorning4 = 0
 
 user = 1
 lineList = []
@@ -17,18 +30,52 @@ for line in lines:
 	time1 = int(split[1])
 	time2 = int(split[2])
 	difference = time2 - time1
-	if time1 > 1459832400 and time2 < 1459854000:
-		totalNight = totalNight + difference
-	if time1 > 1459861200 and time2 < 1459882800:
-		totalMorning = totalMorning + difference
-	print(str(difference) + ' = ' + str(time2) + ' - ' + str(time1))
+
+	# morning 1 6am - noon
+	if time1 > 1464613200 and time2 < 1464656400:
+		totalMorning1 = totalMorning1 + difference
+	#night 1 noon - 6am
+	if time1 > 1464656400 and time2 < 1464699600:
+		totalNight1 = totalNight1 + difference
+	# day 2
+	if time1 > 1464699600 and time2 < 1464742800:
+		totalMorning2 = totalMorning2 + difference
+	if time1 > 1464742800 and time2 < 1464786000:
+		totalNight2 = totalNight2 + difference
+	# day 3
+	if time1 > 1464786000 and time2 < 1464829200:
+		totalMorning3 = totalMorning3 + difference
+	if time1 > 1464829200 and time2 < 1464872400:
+		totalNight3 = totalNight3 + difference
+	# day 4
+	if time1 > 1464872400 and time2 < 1464915600:
+		totalMorning4 = totalMorning4 + difference
+	if time1 > 1464915600 and time2 < 1464958800:
+		totalNight4 = totalNight4 + difference
+
+	# print(str(difference) + ' = ' + str(time2) + ' - ' + str(time1))
 	if user != index:
 		with open('./classifications.csv', 'a') as f:
-			f.write(str(user) + ',' + str(total) + ',' + str(totalNight) + ',' + str(totalMorning) + '\n')
+			f.write(str(user) + ',' + str(total) + ',' 
+				+ str(totalNight1) + ',' 
+				+ str(totalMorning1) + ',' 
+				+ str(totalNight2) + ',' 
+				+ str(totalMorning2) + ',' 
+				+ str(totalNight3) + ',' 
+				+ str(totalMorning3) + ',' 
+				+ str(totalNight4) + ',' 
+				+ str(totalMorning4)
+				+ '\n')
 
 		total = 0
-		totalNight = 0
-		totalMorning = 0
+		totalNight1 = 0
+		totalMorning1 = 0
+		totalNight2 = 0
+		totalMorning2 = 0
+		totalNight3 = 0
+		totalMorning3 = 0
+		totalNight4 = 0
+		totalMorning4 = 0
 		user = index
 
 	total = total + difference
